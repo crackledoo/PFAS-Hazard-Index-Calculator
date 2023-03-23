2418c85f2af7d74ffe50cc969ec69e4d9997d295
Jeremy Wolfe <jeremy.t.wolfe@outlook.com>
2023-03-23 13:43:01 -0400

    Made JavaScript GTM-friendly by (hopefully) removing all ES6 dependencies

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const inputNodeList = document.querySelectorAll('.equation-input');
-const inputArray = Array.from(inputNodeList);
+var inputNodeList = document.querySelectorAll('.equation-input');
+var inputArray = Array.from(inputNodeList);
 
-const inputFieldIds = ['#pfhxs', '#pfna', '#hfpo-da', '#pfbs'];
+var inputFieldIds = ['#pfhxs', '#pfna', '#hfpo-da', '#pfbs'];
 
-const findFieldValue = function(id) {
-  const field = document.querySelector(id);
+var findFieldValue = function(id) {
+  var field = document.querySelector(id);
   return parseFloat(field.value);
 }
 
-const findDenominator = function(id) {
-  const field = document.querySelector(id);
-  const parent = field.parentNode;
-  const grandParent = parent.parentNode;
-  const child = grandParent.querySelector('.denominator');
+var findDenominator = function(id) {
+  var field = document.querySelector(id);
+  var parent = field.parentNode;
+  var grandParent = parent.parentNode;
+  var child = grandParent.querySelector('.denominator');
   return parseFloat(child.innerHTML);
 }
 
-inputArray.forEach(input => input.addEventListener("keyup", () => {
-  let sum = 0;
-  inputFieldIds.forEach(id => {
-    let value = findFieldValue(id);
-    let denominator = findDenominator (id);
-    let weightedValue = value / denominator;
-    if (!isNaN(weightedValue)) {
-      sum = sum + weightedValue;
-    }
-  });
-  const result = document.querySelector('.hi-result');
-  result.innerHTML = sum;
-}));
+inputArray.forEach(function(input) {
+  input.addEventListener("keyup", function() {
+    var sum = 0;
+    inputFieldIds.forEach(function(id) {
+      var value = findFieldValue(id);
+      var denominator = findDenominator (id);
+      var weightedValue = value / denominator;
+      if (!isNaN(weightedValue)) {
+        sum = sum + weightedValue;
+      }
+    });
+    var result = document.querySelector('.hi-result');
+    result.innerHTML = sum;
+  })
+});
